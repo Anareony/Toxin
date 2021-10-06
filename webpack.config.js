3e5db7eeddf9257ad02411609d5b945a1224fc96
Anareony <chudoyakov.roman@gmail.com>
2021-10-06 19:17:43 +0700

    Added pug

--- webpack.config.js
+++ webpack.config.js
@@ -6,24 +6,12 @@ const CopyWebpackPlugin = require ('copy-webpack-plugin')
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js'
+        'uikit': './src/pages/uikit/uikit.js'
     },
     output: {
-        filename: '[name].[contenthash].js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
-    plugins: [
-        new HTMLWebpackPlugin({
-            title:'help meclear'
-        }),
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin({patterns:[
-            {
-                from: path.resolve(__dirname,'./src/favicon'),
-                to: path.resolve(__dirname,'./dist')
-            }]
-        })
-    ],
     module:{
         rules:[
             {
@@ -47,5 +35,18 @@ module.exports = {
                 use:['file-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: "uikit.html",
+            template: './src/pages/uikit/uikit.pug'
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({patterns:[
+            {
+                from: path.resolve(__dirname,'./src/favicon'),
+                to: path.resolve(__dirname,'./dist')
+            }]
+        })
+    ]
 }
