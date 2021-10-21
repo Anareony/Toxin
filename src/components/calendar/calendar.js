e2f189cff94f5ecb1b30ff2b363f6031e2a15106
Anareony <chudoyakov.roman@gmail.com>
2021-10-21 19:32:06 +0700

    Added components (in work)

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,2 +1,10 @@
-import 'air-datepicker'
+import AirDatepicker from 'air-datepicker'
 import 'air-datepicker/air-datepicker.css'
+import './calendar.scss'
+
+new AirDatepicker('#start', {
+    navTitles: {
+        days: '<strong>MMMM</strong> <i>yyyy</i>',
+    },    
+    buttons: ['clear']
+})
