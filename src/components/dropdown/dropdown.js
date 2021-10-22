d4761458f2d5c46782553d251045a5ff44d9394c
Anareony <chudoyakov.roman@gmail.com>
2021-10-23 00:15:54 +0700

    Working w/ dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,3 +1,7 @@
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.js'
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css'
+import './dropdown.scss'
 
+$(document).ready(function() {
+    $(".rooms").iqDropdown();
+});
