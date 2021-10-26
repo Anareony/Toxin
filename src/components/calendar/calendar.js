ab521300342649d7ea340edc48bfbb34ac15481c
Anareony <chudoyakov.roman@gmail.com>
2021-10-27 01:46:10 +0700

    Added calendar(in work)

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -2,7 +2,15 @@ import AirDatepicker from 'air-datepicker'
 import 'air-datepicker/air-datepicker.css'
 import './calendar.scss'
 
-new AirDatepicker('#start', {
+new AirDatepicker('.datepicker__dropdown',{
+    range: true,
+    multipleDatesSeparator: ' - ',
+})
+
+new AirDatepicker('.filter-date', {
+    range: true,
+    multipleDatesSeparator: ' - ',
+    dateFormat: 'dd MMM',
     navTitles: {
         days: '<strong>MMMM</strong> <i>yyyy</i>',
     },    
