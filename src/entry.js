76512213804382a6cdb045e1d98230f62e988549
Anareony <chudoyakov.roman@gmail.com>
2021-10-31 17:04:16 +0700

    fix fonts

---
+++ src/entry.js
@@ -0,0 +1,8 @@
+import './components/calendar/calendar'
+import './components/dropdown/dropdown'
+import './components/range-slider/range-slider'
+
+import './pages/colors-type/colors-type'
+import './pages/form-elements/form-elements'
+import './pages/cards/cards'
+import './pages/headers-footers/headers-footers'
--- src/pages/ui-kit/ui-kit.js
+++
@@ -1,5 +0,0 @@
-import './ui-kit.scss'
-
-import '../../components/calendar/calendar'
-import '../../components/dropdown/dropdown'
-import '../../components/range-slider/range-slider'
